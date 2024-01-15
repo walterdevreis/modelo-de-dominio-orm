471e849c85998df04cf0924e6ffb3d91392f26af
walterdevreis <walter-reis@outlook.com>
2024-01-14 21:53:59 -0300

    Criada e configurada a classe categoria

---
+++ src/main/java/com/devsuperior/modelodedominioorm/entities/Categoria.java
@@ -0,0 +1,38 @@
+package com.devsuperior.modelodedominioorm.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+
+    public Categoria(){}
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}

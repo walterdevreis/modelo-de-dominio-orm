5aff11a52a2b82432f643fe4b828d9d7af6ac0ed
walterdevreis <walter-reis@outlook.com>
2024-01-14 21:34:30 -0300

    Criada a estrutura spring boot com spring web, spring jpa e spring h2

---
+++ src/main/java/com/devsuperior/modelodedominioorm/ModeloDeDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modelodedominioorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloDeDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloDeDominioOrmApplication.class, args);
+	}
+
+}
